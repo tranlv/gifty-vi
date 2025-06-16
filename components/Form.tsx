648ccdd95c3e7463bfe9612665854b9d168faeaa
Tran Ly Vu <tranlyvu.official@gmail.com>
2025-06-16 16:04:39 +0800

    .
    Signed-off-by: Tran Ly Vu <tranlyvu.official@gmail.com>

--- components/Form.tsx
+++ components/Form.tsx
@@ -54,7 +54,7 @@ const Form = () => {
             autoComplete="email"
             aria-invalid="false"
             id="email"
-            placeholder="Enter your email"
+            placeholder="Nhập email để giữ chỗ"
             className="form-control block w-full rounded-sm bg-gray px-4 py-5 text-base text-black placeholder-gray-500 focus:outline-none"
             value={mail}
             onChange={(e) => setMail(e.target.value)}
@@ -66,7 +66,7 @@ const Form = () => {
             className="relative sm:absolute right-2 sm:top-2 w-full sm:w-auto block rounded-sm bg-activeButton py-3 px-4 font-medium text-white shadow hover:bg-activeButton disabled:cursor-not-allowed"
             disabled={mail === "" || loading}
           >
-            Reserve Your Spot
+            Đặt trước suất tham gia
           </button>
         </div>
       </div>
